f9d68ded392469dcf1478b88df703cf9b477322a
YUIMORII <yuimoriistudio@gmail.com>
2023-01-18 10:31:11 -0600

    U

--- ⛳️final_project/api_server/app.js
+++ ⛳️final_project/api_server/app.js
@@ -1,18 +1,24 @@
-// 导入 express 模块
+////////////////////导入所需模块👇
+//1. 导入 express 模块
 const express = require("express");
-const cors = require("cors");
-//创建服务器的实例对象
+//2. 创建服务器的实例对象
 const app = express();
-
-// 导入 cors 中间件
+////////////////////配置cors跨域👇
+// 4.1 导入 cors 中间件
 const cors = require("cors");
-// 将 cors 注册为全局中间件
+// 4.2 将 cors 注册为全局中间件
 app.use(cors());
+////////////////////导入并注册用户路由模块👇
+const userRouter = require("./router/user");
+/* 用app.use 注册为路由模块, /api表示在访问userRouter里面每一个模块的时候, 都必须加入/api前缀 */
+app.use("/api", userRouter);
 
-//配置解析表单数据的中间件
+////////////////////配置解析表单数据的中间件👇
+//4.3 (注意：这个中间件，只能解析application/x-www-form-urlencoded 格式的表单数据)
 app.use(express.urlencoded({ extended: false }));
 
-// 调用 app.listen 方法，指定端口号并启动web服务器
+////////////////////调用 app.listen 方法👇指定端口号并启动web服务器
+//3. 启动服务器
 app.listen(3007, () => {
-  console.log("api server is listening on http://127.0.0.1");
+  console.log("api server running at http://127.0.0.1:3007");
 });
