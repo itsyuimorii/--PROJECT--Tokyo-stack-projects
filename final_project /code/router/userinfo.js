8df1b2395e631c652aa8cc37f416a873144a7d0b
YUIMORII <yuimoriistudio@gmail.com>
2023-01-22 16:00:40 -0600

    Update

--- final_project /code/router/userinfo.js
+++ final_project /code/router/userinfo.js
@@ -1,14 +1,39 @@
-//初始化路由模块
-//导入express
 const express = require("express");
-
-//创建路由对象, 通过调用router
 const router = express.Router();
 
-//获取用户基本信息,挂载路由
-router.get("/userinfo", (req, res) => {
-  res.send("userinfo check");
-});
+// 挂载路由
 
-//向外分享路由对象
+// 导入路由处理函数模块
+const userinfo_handler = require("../router_handler/userinfo");
+
+// 导入验证数据的中间件
+const expressJoi = require("@escook/express-joi");
+// 导入需要的验证规则对象
+const {
+  update_userinfo_schema,
+  update_password_schema,
+  update_avatar_schema,
+} = require("../schema/user");
+
+// 获取用户基本信息的路由
+router.get("/userinfo", userinfo_handler.getUserInfo);
+// 更新用户信息的路由
+router.post(
+  "/userinfo",
+  expressJoi(update_userinfo_schema),
+  userinfo_handler.updateUserInfo
+);
+// 更新密码的路由
+router.post(
+  "/updatepwd",
+  expressJoi(update_password_schema),
+  userinfo_handler.updatePassword
+);
+// 更换头像的路由
+router.post(
+  "/update/avatar",
+  expressJoi(update_avatar_schema),
+  userinfo_handler.updateAvatar
+);
+
 module.exports = router;
