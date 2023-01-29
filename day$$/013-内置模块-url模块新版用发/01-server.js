de91367924347d0450f214569c11630413c002cc
YUIMORII <yuimoriistudio@gmail.com>
2023-01-28 21:20:28 -0600

    Update querystring

--- day$$/013-内置模块-url模块新版用发/01-server.js
+++ day$$/013-内置模块-url模块新版用发/01-server.js
@@ -1,57 +1,53 @@
-/*
- * @作者: kerwin
- * @公众号: 大前端私房菜
- */
-var http = require("http")
-var url = require("url")
-const { fileURLToPath ,urlToHttpOptions} = require('url');
+var http = require("http");
+var url = require("url");
+const { fileURLToPath, urlToHttpOptions } = require("url");
 
-var moduleRenderHTML = require("./module/renderHTML")
-var moduleRenderStatus = require("./module/renderStatus")
+var moduleRenderHTML = require("./module/renderHTML");
+var moduleRenderStatus = require("./module/renderStatus");
 //创建服务器
 
-var server = http.createServer()
+var server = http.createServer();
 
-server.on("request",(req,res)=>{
-    //req 接受浏览器传的参数 
-    //res 返回渲染的内容
+server.on("request", (req, res) => {
+  //req 接受浏览器传的参数
+  //res 返回渲染的内容
 
-    // res.write("hello wolrd")
-    // res.write("hello wolrd22")
-    // res.end([1,2,3]) 
+  // res.write("hello wolrd")
+  // res.write("hello wolrd22")
+  // res.end([1,2,3])
 
-    // res.write("aaaaa")
-    if(req.url==="/favicon.ico"){
-        // todo 读取本地图标
-        return
-    }
-    // console.log(url.parse(req.url).pathname,"2222")
-    // var urlobj = url.parse(req.url,true)
-    // console.log(urlobj.query.name,urlobj.query.age)
-    // var pathname = url.parse(req.url).pathname
+  // res.write("aaaaa")
+  if (req.url === "/favicon.ico") {
+    // todo 读取本地图标
+    return;
+  }
+  // console.log(url.parse(req.url).pathname,"2222")
+  // var urlobj = url.parse(req.url,true)
+  // console.log(urlobj.query.name,urlobj.query.age)
+  // var pathname = url.parse(req.url).pathname
 
-    const myURL = new URL(req.url, 'http://127.0.0.1:3000');
+  const myURL = new URL(req.url, "http://127.0.0.1:3000");
 
-    console.log(myURL.searchParams.get("a"))
-    for(var [key,value] of myURL.searchParams){
-        console.log(key,value)
-    }
-    const pathname = myURL.pathname
-    res.writeHead(moduleRenderStatus.renderStatus(pathname),{"Content-Type":"text/html;charset=utf-8"})
-    res.write(moduleRenderHTML.renderHTML(pathname))
-    res.end()
-})
+  console.log(myURL.searchParams.get("a"));
+  for (var [key, value] of myURL.searchParams) {
+    console.log(key, value);
+  }
+  const pathname = myURL.pathname;
+  res.writeHead(moduleRenderStatus.renderStatus(pathname), {
+    "Content-Type": "text/html;charset=utf-8",
+  });
+  res.write(moduleRenderHTML.renderHTML(pathname));
+  res.end();
+});
 
-server.listen(3000,()=>{
-    console.log("server start")
-})
-
+server.listen(3000, () => {
+  console.log("server start");
+});
 
 // const url = require('url')
 // const urlString = 'https://www.baidu.com:443/ad/index.html?id=8&name=mouse#tag=110'
 // const parsedStr = url.parse(urlString)
 // console.log(parsedStr)
-
 
 // const urlObject = {
 //     protocol: 'https:',
@@ -69,22 +65,26 @@ server.listen(3000,()=>{
 
 //   console.log(url.format(urlObject))
 
-
-// var a = url.resolve('/one/two/three/', 'four') 
+// var a = url.resolve('/one/two/three/', 'four')
 // console.log(a)
 
-var b = new URL('/one', 'http://example.com/aaaa/bbbbb/')
+var b = new URL("/one", "http://example.com/aaaa/bbbbb/");
 
-console.log(b.href) 
+console.log(b.href);
 
+const myURL = new URL("https://a:b@測試?abc#foo");
+console.log(
+  url.format(myURL, {
+    unicode: true,
+    auth: false,
+    fragment: false,
+    search: false,
+  })
+);
 
-const myURL = new URL('https://a:b@測試?abc#foo');
-console.log(url.format(myURL,{unicode:true,auth:false,fragment:false,search:false}));
+console.log(new URL("file://c://你好.txt").pathname); // 错误: /%E4%BD%A0%E5%A5%BD.txt
+console.log(fileURLToPath("file://c://你好.txt")); // 正确: /你好.txt (POSIX)
 
-console.log(new URL('file://c://你好.txt').pathname);      // 错误: /%E4%BD%A0%E5%A5%BD.txt
-console.log(fileURLToPath('file://c://你好.txt'));         // 正确: /你好.txt (POSIX) 
-
-
-const myURL2 = new URL('https://a:b@測試?abc#foo');
-console.log(myURL2)
+const myURL2 = new URL("https://a:b@測試?abc#foo");
+console.log(myURL2);
 console.log(urlToHttpOptions(myURL2));
