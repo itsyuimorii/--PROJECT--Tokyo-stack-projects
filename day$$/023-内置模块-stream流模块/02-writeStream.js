96ed19a931bd4dfbc80bde011211c4e310ef92de
YUIMORII <yuimoriistudio@gmail.com>
2023-01-29 11:44:19 -0600

    update route

---
+++ day$$/023-内置模块-stream流模块/02-writeStream.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+const ws = fs.createWriteStream("./2.txt", "utf-8");
+ws.write("1111111111111111111");
+ws.write("22222222222222222222");
+ws.write("33333333333");
+
+ws.end();
