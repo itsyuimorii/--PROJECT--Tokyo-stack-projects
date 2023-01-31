51cecb4ba04ce173fc5fcb01670c2f989056fc20
YUIMORII <yuimoriistudio@gmail.com>
2023-01-30 21:01:57 -0600

    update

--- itqf-2/035-express-服务端渲染与客户端渲染-1/route/HomeRouter.js
+++ itqf-2/035-express-服务端渲染与客户端渲染-1/route/HomeRouter.js
@@ -1,26 +1,21 @@
-/*
- * @作者: kerwin
- * @公众号: 大前端私房菜
- */
-const express = require("express")
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 //路由级别
-router.get("/",(req,res)=>{
-    res.send("home")
-})
+router.get("/", (req, res) => {
+  res.send("home");
+});
 
-router.get("/list",(req,res)=>{
-    res.send(["111","222","333"])
-})
+router.get("/list", (req, res) => {
+  res.send(["111", "222", "333"]);
+});
 
+router.get("/swiper", (req, res) => {
+  res.send("home-swiper");
+});
 
-router.get("/swiper",(req,res)=>{
-    res.send("home-swiper")
-})
+router.get("/slide", (req, res) => {
+  res.send("home-slide");
+});
 
-router.get("/slide",(req,res)=>{
-    res.send("home-slide")
-})
-
-module.exports=  router
+module.exports = router;
---
+++ itqf-3/4. project-admin/README.md
@@ -0,0 +1,21 @@
+### Frontend
+
+- Front-end engineering environment (webpack）
+- CSS（sass）
+- JS Modularity: ES Module, CommonJS Module
+- JS libeary：jQuery
+- SPA：single page application，路由：SME-Router
+- UI Component library: Bootstrap(AdminLTE)
+- RMVC: Art-template
+
+### Backend
+
+- Node.js
+- Express(static, Router, randomstring, bcrypt, cookie-session)
+- MongoDB (Mongoose)
+- EJS
+- jwt(json web token)
+- RMVP
+  Component library: Bootstrap
+
+###
