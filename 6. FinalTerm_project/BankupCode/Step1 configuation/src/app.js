a7eb2b40d8a30f8ef57958912d0ed2dbcc7e43f5
YUIMORII <yuimoriistudio@gmail.com>
2023-02-02 11:59:46 -0600

    Update finalterm

--- 6. FinalTerm_project/BankupCode/Step1 configuation/src/app.js
+++
@@ -1,9 +0,0 @@
-import { $dataMetaSchema } from "ajv";
-import indexTpl from "./views/index.art";
-import signInTpl from "./views/signin.art";
-
-// const html = indexTpl({});
-// console.log(html);
-const html = signInTpl({});
-
-$("#root").html(html);
