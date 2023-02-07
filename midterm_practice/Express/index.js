883d36594a8abcbd481972e0c7fa2e57b2bf183b
YUIMORII <yuimoriistudio@gmail.com>
2023-02-07 15:13:17 -0600

    update

--- midterm_practice/Express/index.js
+++ midterm_practice/Express/index.js
@@ -4,20 +4,18 @@ const path = require("path");
 // 创建服务器的实例
 const app = express();
 
-// Static Middleware
-// After setting the static middleware, the browser will automatically go to the public directory to find out if there are any matching static resources when it visits
+app.use(express.static(path.join(__dirname, "./public")));
 
-// app.use(express.static(path.join(__dirname, "public")));
-
-app.use(express.static(path.resolve(__dirname, "./public")));
-
-app.get("/home", function (req, res, next) {
-  res.send(200);
-});
-
-//ejs file has to be created under views folder
-app.get("/", function (req, res, next) {
-  res.render("home.ejs");
+app.get("/login"),
+  (req, res) => {
+    //Get the username and password entered by the user
+    if (req.query.username === "admin" && req.query.password === "123456") {
+      res.send("<h1>Login successful！</h1>");
+    } else {
+      res.send("<h1>Wrong username or password!</h1>");
+    }
+  };
+app.get("/hello,(req, res) => {
 });
 app.listen(3000, () => {
   console.log("App is listening on port http://localhost:3000 ");
