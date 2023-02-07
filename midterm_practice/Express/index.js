2a0938eac76542800062301ba2e9275e02c1b8a8
YUIMORII <yuimoriistudio@gmail.com>
2023-02-07 13:31:16 -0600

    Update index.js

--- midterm_practice/Express/index.js
+++ midterm_practice/Express/index.js
@@ -1,17 +1,13 @@
 const express = require("express");
 
 const app = express();
-const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.status(200);
-  res.send("Welcome to root URL of Server");
+app.get("/hello", (req, res) => {
+  res.send("this is hello router");
 });
 
-app.listen(PORT, (error) => {
-  if (!error)
-    console.log(
-      "Server is Successfully Running,and App is listening on port " + PORT
-    );
-  else console.log("Error occurred, server can't start", error);
+app.listen(3000, (error) => {
+  console.log(
+    "Server is Successfully Running,and App is listening on port http://localhost:3000"
+  );
 });
