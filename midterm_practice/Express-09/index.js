36e5f1ab809eb4b42d171debeabeb9e761974a2f
YUIMORII <yuimoriistudio@gmail.com>
2023-02-07 22:41:23 -0600

    Update express.js

--- midterm_practice/Express-09/index.js
+++ midterm_practice/Express-09/index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-//配置静态资源路径
+// Configure static resource paths
 app.use(express.static(path.resolve(__dirname, "public")));
 
+// Processing request body parsing
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/hello", (req, res) => {
+  res.send("hello");
+});
+
+//Configure error routes, need to be under all routes
+app.use((req, res) => {
+  res.status(404);
+  res.send("The address you are visiting has been hijacked by aliens");
+});
+
 app.listen(3000, () => {
-  console.log("listening 鸥鸟h");
+  console.log("listening on http://localhost:3000");
 });
