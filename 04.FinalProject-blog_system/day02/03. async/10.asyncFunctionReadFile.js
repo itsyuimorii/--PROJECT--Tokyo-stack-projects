663f219ca7716691c2b906d07b667b4595664f88
YUIMORII <yuimoriistudio@gmail.com>
2023-02-12 10:45:15 -0600

    update async

--- 04.FinalProject-blog_system/day02/03. async/10.asyncFunctionReadFile.js
+++ 04.FinalProject-blog_system/day02/03. async/10.asyncFunctionReadFile.js
@@ -1,16 +1,16 @@
-const fs = require('fs');
-// 改造现有异步函数api 让其返回promise对象 从而支持异步函数语法
-const promisify = require('util').promisify;
-// 调用promisify方法改造现有异步API 让其返回promise对象
+const fs = require("fs");
+// Modify the existing asynchronous function api to return a promise object, thus supporting asynchronous function syntax
+const promisify = require("util").promisify;
+// call the promisify method to adapt the existing asynchronous API to return a promise object
 const readFile = promisify(fs.readFile);
 
-async function run () {
-	let r1 = await readFile('./1.txt', 'utf8')
-	let r2 = await readFile('./2.txt', 'utf8')
-	let r3 = await readFile('./3.txt', 'utf8')
-	console.log(r1)
-	console.log(r2)
-	console.log(r3)
+async function run() {
+  let r1 = await readFile("./1.txt", "utf8");
+  let r2 = await readFile("./2.txt", "utf8");
+  let r3 = await readFile("./3.txt", "utf8");
+  console.log(r1);
+  console.log(r2);
+  console.log(r3);
 }
 
 run();
--- 04.FinalProject-blog_system/day02/03. async/5.asyncOrder.js
+++ 04.FinalProject-blog_system/day02/03. async/5.asyncOrder.js
@@ -1,11 +1,11 @@
-console.log('代码开始执行')
+console.log("代码开始执行");
 
 setTimeout(function () {
-	console.log('2s')
-}, 2000)
+  console.log("2s");
+}, 2000);
 
 setTimeout(function () {
-	console.log('0s')
-}, 0)
+  console.log("0s");
+}, 0);
 
-console.log('代码结束执行')
+console.log("Code ended execution");
