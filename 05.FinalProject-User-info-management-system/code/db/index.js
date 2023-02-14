32245d5776a13116bed3a66ae2f76540d4593353
YUIMORII <yuimoriistudio@gmail.com>
2023-02-14 10:49:46 -0600

    Update filename

--- 05.FinalProject-User-info-management-system/code/db/index.js
+++ 05.FinalProject-User-info-management-system/code/db/index.js
@@ -1,13 +1,13 @@
 //导入mysql 模块
 const mysql = require("mysql");
 
-// 2. 建立与 MySQL 数据库的连接关系~
+// 2. Create a connection to the MySQL database ~
 const db = mysql.createPool({
-  host: "127.0.0.1", // 数据库的 IP 地址
-  user: "root", // 登录数据库的账号
-  password: "123456", // 登录数据库的密码
-  database: "blog_db_2023", // 指定要操作哪个数据库
+  host: "127.0.0.1", // the IP address of the database
+  user: "root", // account to log in to the database
+  password: "123456", // password to log in to the database
+  database: "blog_db_2023", // Specify which database to operate
 });
 
-// 向外共享 db 数据库连接对象
+// Share the db database connection object to the outside
 module.exports = db;
