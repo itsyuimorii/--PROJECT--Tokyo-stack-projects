fb10abe51fc7beed907bc967edaea2529c728794
YUIMORII <yuimoriistudio@gmail.com>
2023-02-14 10:36:10 -0600

    update

--- 04.FinalProject-blog_system/router/admin.js
+++ 04.FinalProject-blog_system/router/admin.js
@@ -4,5 +4,9 @@ const express = require("express");
 //create routes for blog administration
 const admin = express.Router();
 
+admin.get("/login", (req, res) => {
+  res.render("");
+});
+
 // Export the routing object as a member of the routing module
 module.exports = admin;
