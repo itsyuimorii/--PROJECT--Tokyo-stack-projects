aa3baf33ff7cd3f431c5097d02f3da7cfaa27773
YUIMORII <yuimoriistudio@gmail.com>
2023-02-16 14:54:50 -0600

    U

--- 05.FinalProject-blog_management_system/router/admin.js
+++ 05.FinalProject-blog_management_system/router/admin.js
@@ -6,16 +6,17 @@ const session = require("express-session");
 //create routes for blog administration
 const admin = express.Router();
 
-//渲染登錄頁面
+// Render the login page
 admin.get("/login", require("./admin/loginPage"));
 
-// Create a user list route, this route will be based on the "Level 1 route" -admin "Level 2 route"
-admin.get("/user", (req, res) => {
-  res.render("admin/user");
-});
-
-//login routes
+// Realization of registration function
 admin.post("/login", require("./admin/login"));
+
+// Create a user list route
+//this route will be based on the "Level 1 route" -admin "Level 2 route"
+admin.get("/user", require("./admin/userPage"));
+
+admin.get("/logout", require("./admin/logout"));
 
 // Export the routing object as a member of the routing module
 module.exports = admin;
