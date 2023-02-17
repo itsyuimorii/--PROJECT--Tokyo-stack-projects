1d8e879df4618279e83283de28531b383e07098a
YUIMORII <yuimoriistudio@gmail.com>
2023-02-16 21:45:10 -0600

    U

--- 05.FinalProject-blog_management_system/Step02_addUser/router/admin/userEdit-fn.js
+++ 05.FinalProject-blog_management_system/Step02_addUser/router/admin/userEdit-fn.js
@@ -1,6 +1,17 @@
+const Joi = require("joi");
+
 module.exports = (req, res) => {
   //這裡實現用戶添加功能
   // res.send("ok");
   res.send(req.body);
   //{"username":"matthew","email":"matthew@gmail.com","password":"000000","state":"0"}
+
+  //定義對象的驗證規則
+  const schema = {
+    username: Joi.string()
+      .min(2)
+      .max(12)
+      .required()
+      .error(new Error("Invalid username")),
+  };
 };
