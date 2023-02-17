3cb14fc78646214508790cf1c259ebaf93589d90
YUIMORII <yuimoriistudio@gmail.com>
2023-02-17 15:06:36 -0600

    U

--- 05.FinalProject-blog_management_system/Step02_addUser/router/admin/userList.js
+++ 05.FinalProject-blog_management_system/Step02_addUser/router/admin/userList.js
@@ -1,3 +1,12 @@
-module.exports = (req, res) => {
-  res.render("admin/user");
+//導入用戶結合構造函數
+const { User } = require("../../model/user");
+module.exports = async (req, res) => {
+  // Query the user information from the database
+  //users 接受返回的結果
+  let users = await User.find({});
+  //渲染頁面模板, 將接收到的結果users傳入模板中, users是數組
+  res.send(users);
+  //   res.render("admin/user", {
+  //     users: users,
+  //   });
 };
--- 05.FinalProject-blog_management_system/Step02_addUser/router/admin/userPage.js
+++
@@ -1,5 +0,0 @@
-const { User } = require("../../model/user");
-module.exports = (req, res) => {
-  // Query the user information from the database
-  res.render("admin/user");
-};
