4ae4ac6f67928a794b76bbb322daf56c9e297e30
YUIMORII <yuimoriistudio@gmail.com>
2023-02-17 10:07:26 -0600

    update

--- 05.FinalProject-blog_management_system/Step02_addUser/router/admin/userEdit-fn.js
+++ 05.FinalProject-blog_management_system/Step02_addUser/router/admin/userEdit-fn.js
@@ -7,11 +7,28 @@ module.exports = (req, res) => {
   //{"username":"matthew","email":"matthew@gmail.com","password":"000000","state":"0"}
 
   //定義對象的驗證規則
+
   const schema = {
     username: Joi.string()
       .min(2)
       .max(12)
       .required()
       .error(new Error("Invalid username")),
+    email: Joi.string().email().required().error(new Error("Invalid email")),
+    password: Joi.string()
+      .regex(/^[a-zA-Z0-9]{3,30}$/)
+      .required()
+      .error(new Error("Invalid password")),
+    role: Joi.string()
+      .valid("normal", "admin")
+      .required()
+      .error(new Error("Invalid Value")),
+    state: Joi.number()
+      .valid(0, 1)
+      .required()
+      .error(new Error("Invalid status")),
   };
+
+  // 实施验证
+  Joi.validate(req.body, schema);
 };
