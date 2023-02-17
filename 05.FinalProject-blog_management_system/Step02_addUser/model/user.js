44d3dbe4beecc4d4e8a7e7401c5895eed5bca544
YUIMORII <yuimoriistudio@gmail.com>
2023-02-17 12:25:28 -0600

    U

--- 05.FinalProject-blog_management_system/Step02_addUser/model/user.js
+++ 05.FinalProject-blog_management_system/Step02_addUser/model/user.js
@@ -59,6 +59,36 @@ async function createUser() {
     console.log("error creating user");
   }); */
 
+// 验证用户信息
+const validateUser = (user) => {
+  // 定义对象的验证规则
+  const schema = {
+    username: Joi.string()
+      .min(2)
+      .max(12)
+      .required()
+      .error(new Error("用户名不符合验证规则")),
+    email: Joi.string()
+      .email()
+      .required()
+      .error(new Error("邮箱格式不符合要求")),
+    password: Joi.string()
+      .regex(/^[a-zA-Z0-9]{3,30}$/)
+      .required()
+      .error(new Error("密码格式不符合要求")),
+    role: Joi.string()
+      .valid("normal", "admin")
+      .required()
+      .error(new Error("角色值非法")),
+    state: Joi.number().valid(0, 1).required().error(new Error("状态值非法")),
+  };
+
+  // 实施验证
+  return Joi.validate(user, schema);
+};
+
+// 将用户集合做为模块成员进行导出
 module.exports = {
-  User: User,
+  User,
+  validateUser,
 };
