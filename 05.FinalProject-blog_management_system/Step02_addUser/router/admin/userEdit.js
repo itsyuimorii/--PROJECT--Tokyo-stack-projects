25391b1e434feda6bd9b788eb4a51f583725e213
YUIMORII <yuimoriistudio@gmail.com>
2023-02-19 10:55:41 -0600

    U

--- 05.FinalProject-blog_management_system/Step02_addUser/router/admin/userEdit.js
+++ 05.FinalProject-blog_management_system/Step02_addUser/router/admin/userEdit.js
@@ -2,9 +2,18 @@
 const { User } = require("../../model/user"); // 引入加密模块
 
 module.exports = async (req, res) => {
-  const { message } = req.query;
+  //獲取到地址欄中的id參數
+
+  const { message, id } = req.query;
   // 添加操作
   res.render("admin/userEdit", {
     message: message,
   });
+
+  //如果當前傳遞了id參數,
+  if (id) {
+    //修改操作
+  } else {
+    //添加操作
+  }
 };
