f047a453a632ff1a8ed4235634f1838fe73ed551
YUIMORII <yuimoriistudio@gmail.com>
2023-02-20 11:54:57 -0600

    Update userEdit.js

--- 05.FinalProject-blog_management_system/Step03_editUser/router/admin/userEdit.js
+++ 05.FinalProject-blog_management_system/Step03_editUser/router/admin/userEdit.js
@@ -6,9 +6,8 @@ module.exports = async (req, res) => {
 
   const { message, id } = req.query;
 
-  //如果當前傳遞了id參數,
+  //如果當前傳遞了id參數, then, it is edit user info
   if (id) {
-    //修改操作
     let user = await User.findOne({ _id: id });
     //return res.send(user);
 
