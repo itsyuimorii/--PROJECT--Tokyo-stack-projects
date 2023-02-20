e7f40ed827048f0ed35237a6e3a77962cef1fe21
YUIMORII <yuimoriistudio@gmail.com>
2023-02-20 13:29:09 -0600

    Update user-modify.js

--- 05.FinalProject-blog_management_system/Step03_editUser/router/admin/user-modify.js
+++ 05.FinalProject-blog_management_system/Step03_editUser/router/admin/user-modify.js
@@ -1,3 +1,9 @@
 module.exports = (req, res) => {
-  res.send("ok");
+  //res.send("ok");
+  //接受客戶端傳遞過來的請求參數
+  const body = req.body;
+  //req.query拿到id, id是即將要修改的用戶的id
+  const id = req.query.id;
+
+  res.send(body.password);
 };
