1f19eb05b8e66653000c19c2d59547a96900a97e
YUIMORII <yuimoriistudio@gmail.com>
2023-02-20 11:50:37 -0600

    Update 03

---
+++ 05.FinalProject-blog_management_system/Step03_editUser/router/admin/userEdit.js
@@ -0,0 +1,29 @@
+// 引入用户集合的构造函数
+const { User } = require("../../model/user"); //
+
+module.exports = async (req, res) => {
+  //獲取到地址欄中的id參數
+
+  const { message, id } = req.query;
+  // 添加操作
+
+  //如果當前傳遞了id參數,
+  if (id) {
+    //修改操作
+    let user = await User.findOne({ _id: id });
+    //return res.send(user);
+
+    //渲染用戶編輯頁面(修改)
+    res.render("admin/userEdit", {
+      message: message,
+      user: user,
+      link: "/admin/user-add",
+    });
+  } else {
+    //添加操作
+    res.render("admin/userEdit", {
+      message: message,
+      link: "/admin/userEdit",
+    });
+  }
+};
---
+++ 05.FinalProject-blog_management_system/Step03_editUser/router/admin/userList.js
@@ -0,0 +1,27 @@
+//導入用戶結合構造函數
+const { User } = require("../../model/user");
+module.exports = async (req, res) => {
+  //Pagination
+  // 1. Receive the current page parameters from the client
+  let page = req.query.page;
+  //2. Number of data items displayed per page
+  let pagesize = 2;
+  //3. Query the total number of user data
+  let count = await User.countDocuments({});
+  // res.send("用戶的總數是: " + count);
+  // return;
+  //4. Total number of pages
+  let total = Math.ceil(count / pagesize);
+
+  let start = (page - 1) * pagesize;
+  // Query the user information from the database
+  let users = await User.find({}).limit(pagesize).skip(start);
+
+  //res.send(users);
+  //Render the user list template, and pass the received result users into the template, users is an array
+  res.render("admin/user", {
+    users: users,
+    page: page,
+    total: total,
+  });
+};
