54fd1be0444d7e43155d04b6b6cb1afc1dfc9245
YUIMORII <yuimoriistudio@gmail.com>
2023-02-22 13:31:40 -0600

    Update article-add.js

--- 05.FinalProject-blog_management_system/Step04_articleManagement/router/admin/article-add.js
+++ 05.FinalProject-blog_management_system/Step04_articleManagement/router/admin/article-add.js
@@ -1,5 +1,8 @@
 const formidable = require("formidable");
 
 module.exports = (req, res) => {
+  //1. 創建表單解析對象
+  const form = new formidable.IncomingForm();
+  //2. 指定上傳文件的路徑
   res.send("ok");
 };
