5892785f796c4b2791abe8ab97630a08db0762a1
YUIMORII <yuimoriistudio@gmail.com>
2023-02-28 12:04:31 -0600

    Update file

--- 05.FinalProject-blog_management_system/Step04_articleManagement/model/connect.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-// Connecting to the database
-mongoose.set("strictQuery", false),
-  mongoose
-    .connect("mongodb://localhost/blog", { useNewUrlParser: true })
-    .then(() => console.log("Database connection successful"))
-    .catch(() => console.log("Database connection failure"));
